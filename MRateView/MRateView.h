0b64493c74f0d923b56ee877bbc76226f558d4a5
mahdi <notron021@gmail.com>
2019-01-28 11:13:40 +0330

    Initial Commit

---
+++ MRateView/MRateView.h
@@ -0,0 +1,19 @@
+//
+//  MRateView.h
+//  MRateView
+//
+//  Created by Mahdi on 1/28/19.
+//  Copyright © 2019 Mahdi Moayeri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MRateView.
+FOUNDATION_EXPORT double MRateViewVersionNumber;
+
+//! Project version string for MRateView.
+FOUNDATION_EXPORT const unsigned char MRateViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRateView/PublicHeader.h>
+
+
